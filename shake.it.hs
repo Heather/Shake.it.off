9f3d54b7de8211e5654dc2c4abf2ce79e6965fbd
Heather <heather@live.ru>
2016-01-21 11:01:27 +0400

    add test to travis

---
+++ shake.it.hs
@@ -0,0 +1,24 @@
+import System.Exit
+import System.Process
+
+import Data.List
+
+import Control.Monad
+
+import Shake.It.Off
+
+checkExitCode :: ExitCode -> IO ()
+checkExitCode ExitSuccess = return ()
+checkExitCode (ExitFailure code) =
+    error $ "failed with exit code: " ++ (show code)
+
+cabal :: [String] -> IO ()
+cabal a = rawSystem "cabal" a >>= checkExitCode
+
+main :: IO ()
+main = shake $ do
+    pony "clean" $ cabal ["clean"]
+
+    cabal ["install", "--only-dependencies"]
+    cabal ["configure"]
+    cabal ["build"]
