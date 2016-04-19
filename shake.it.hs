1f148dae6a7abfe0a28ecae3f7ae8108b4579cd3
Heather <heather@live.ru>
2016-04-19 14:03:31 +0400

    minor shake.it.hs improvements

--- shake.it.hs
+++ shake.it.hs
@@ -10,19 +10,22 @@ main = shake $ do
   "clean" ∫ cabal ["clean"]
 
   -- building object rule #> is non-unicode operator alternative
-  buildPath </> "shake.exe" ♯ do
+  shakeExecutable ♯ do
     cabal ["install", "--only-dependencies"]
     cabal ["configure"]
     cabal ["build"]
 
   -- install phony depending on obj, @@> is non-unicode operator alternative
   -- ##> or ♯♯ is for dependent object rule, ◉ is just uncarry operator
-  "install" ◉ [buildPath </> "shake.exe"] ∰
+  "install" ◉ [shakeExecutable] ∰
     cabal ["install"]
 
-  "test" ◉ [buildPath </> "shake.exe"] ∰ do
-    rawSystem (buildPath </> "shake.exe") ["--version"]
+  "test" ◉ [shakeExecutable] ∰ do
+    rawSystem shakeExecutable ["--version"]
       >>= checkExitCode
 
  where buildPath :: String
        buildPath = "dist/build/Shake"
+
+       shakeExecutable :: String
+       shakeExecutable = buildPath </> "shake.exe"
