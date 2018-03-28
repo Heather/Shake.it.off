2363edbd1d283f8b9d45491a9be7193cd2d8b5d5
Cynede <cynede@gentoo.org>
2018-03-29 00:53:13 +0400

    Support working with stack way of installing GHC on Windows

--- src/Shake.hs
+++ src/Shake.hs
@@ -16,11 +16,63 @@ import           Shake.It.Off
 import           System.IO
 
 import           Data.String.Utils
+import           Data.Maybe
 
 import           Control.Concurrent
 import           Control.Eternal
 import           Control.Exception
 import           Control.Monad
+
+#if ( defined(mingw32_HOST_OS) || defined(__MINGW32__) )
+readCheck    -- return whether command was success or not
+  ∷ String   -- command
+  → [String] -- arguments
+  → IO (Either SomeException String)
+readCheck γ args = try $ readProcess γ args []
+
+checkIfSucc           -- check if success
+  ∷ String            -- command to check
+  → [String]          -- arguments
+  → IO (Maybe String) -- Just cmd in case of success
+checkIfSucc γ args =
+  readCheck γ args
+    ≫= \case Left _ → return Nothing
+             Right val → do putStr $ γ ⧺ " : " ⧺ val
+                            return (Just γ)
+
+versionCheck          -- check for ghc --version
+  ∷ String            -- command to check
+  → IO (Maybe String) -- Path to GHC in case of success
+versionCheck γ = checkIfSucc γ ["--version"]
+
+checkForStackGHC
+  ∷ Maybe String
+  → IO (Maybe String)
+checkForStackGHC γ =
+  if isNothing γ
+    then do
+      localAppData ← getEnv("LOCALAPPDATA")
+      let path = localAppData </> "Programs/stack/x86_64-windows/"
+      stackPackages ← getDirectoryContents path
+      let ghcPackages = filter (startswith "ghc") stackPackages
+      stackGHV ←
+        case ghcPackages of
+          [] → do appData ← getEnv("APPDATA")
+                  return $ appData </> "local/bin/ghc.exe"
+          xs → let lastGHC = last xs
+               in return $ path </> lastGHC </> "bin/ghc.exe"
+      versionCheck stackGHV
+    else return γ
+
+getGHC ∷ IO String
+getGHC = return Nothing ≫= λ "ghc"
+                        ≫= checkForStackGHC
+                        ≫= \res → return $ fromMaybe "ghc" res
+  where λ ∷ String → Maybe String → IO (Maybe String)
+        λ χ prev = if isNothing prev
+                      then versionCheck χ
+                      else return prev
+#endif
 
 shakeIt ∷ [String]
         → String  -- current directory
@@ -48,6 +100,13 @@ shakeItOff ∷ [String]
            → String   -- shake file
            → IO ()
 shakeItOff args dir force pretend platform shakefile = do
+#if ( defined(mingw32_HOST_OS) || defined(__MINGW32__) )
+  -- on Windows GHC is not possibly in path (specially with stack)
+  -- however we can look for it inside stack packages
+  ghcCommand ← getGHC
+#else
+  let ghcCommand = "ghc"
+#endif
   let cscr = if | platform ∈ ["Win_x64", "Win"] → "shake.it.off.exe"
                 | otherwise → "shake.it.off"
 
@@ -60,7 +119,7 @@ shakeItOff args dir force pretend platform shakefile = do
           return $ cscrMTime <= scrMTime
        | otherwise → return True
 
-  when doRecompile $ system ("ghc --make -o " ++ cscr ++ " " ++ shakefile)
+  when doRecompile $ system (ghcCommand ++ " --make -o " ++ cscr ++ " " ++ shakefile)
                    >>= \case ExitFailure ε → do
                                hPrint stderr ε
                                exitFailure
